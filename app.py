3222cdfead9c0a37cb6803b1afefd7e2d8ea5a60
Jordan Smith <jssmith9876@yahoo.com>
2021-04-24 23:35:05 -0700

    Implemented correct reading from JSON, and started fixing styling

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index_serve():
     return render_template("index.html")
+
+@app.route('/test.json')
+def get_data():
+    with open('static/data/test.json', 'r') as json_file:
+        data = json.load(json_file)
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)

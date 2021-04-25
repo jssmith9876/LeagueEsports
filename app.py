dac701f63967edf1692a0469a81228b014e053fb
Jordan Smith <jssmith9876@yahoo.com>
2021-04-24 23:37:33 -0700

    Added comments

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import json
 
 app = Flask(__name__)
 
+# Default page => index.html
 @app.route('/')
 def index_serve():
     return render_template("index.html")
 
+# When the app needs to load json => send the data 
 @app.route('/test.json')
 def get_data():
     with open('static/data/test.json', 'r') as json_file:
